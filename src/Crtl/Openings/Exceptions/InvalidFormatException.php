db380310e1c981dd3b2fd0f6e0b0a768b6a9a224
Marvin Petker <marvin.petker@borntocreate.de>
2017-03-07 17:50:09 +0100

    Add InvalidFormatException.

---
+++ src/Crtl/Openings/Exceptions/InvalidFormatException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Crtl\Openings\Exceptions;
+
+/**
+ * InvalidFormatException to be thrown when openings with invalid format are passed to openings manager
+ *
+ * @author Marvin Petker
+ */
+class InvalidFormatException extends \Exception {}
